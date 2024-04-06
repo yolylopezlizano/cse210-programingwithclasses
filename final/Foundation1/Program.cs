54e5201f565bb38b9934c62662470aaee6161371
yolylizano <yolylizano@gmail.com>
2024-04-05 21:41:00 -0500

    Fundation 1 and 2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Create videos
+        Video video1 = new Video("Title Video 1", "Author 1", 180);
+        video1.AddComment("Commenter 1", "This is a great video!");
+        video1.AddComment("Commenter 2", "I love it!");
+        video1.AddComment("Commenter 3", "Nice content!");
+
+        Video video2 = new Video("Title Video 2", "Author 2", 240);
+        video2.AddComment("Commenter 1", "Awesome video!");
+        video2.AddComment("Commenter 2", "Interesting stuff.");
+        video2.AddComment("Commenter 3", "Great job!");
+
+        Video video3 = new Video("Title Video 3", "Author 3", 300);
+        video3.AddComment("Commenter 1", "Well done!");
+        video3.AddComment("Commenter 2", "I enjoyed watching this.");
+        video3.AddComment("Commenter 3", "Keep it up!");
+        video3.AddComment("Commenter 4", "Excellent work!");
+
+        // List of videos
+        List<Video> videos = new List<Video> { video1, video2, video3 };
+
+        // Display information for each video
+        foreach (var video in videos)
+        {
+            Console.WriteLine("Title: " + video.Title);
+            Console.WriteLine("Author: " + video.Author);
+            Console.WriteLine("Length: " + video.Length + " seconds");
+            Console.WriteLine("Number of Comments: " + video.GetNumberOfComments());
+            Console.WriteLine("Comments:");
+            foreach (var comment in video.GetComments())
+            {
+                Console.WriteLine($" - {comment.Name}: {comment.Text}");
+            }
+            Console.WriteLine();
+        }
     }
 }
